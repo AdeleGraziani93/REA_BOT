91e6a4ba0584c4a380604ac51279157a7ccc3b47
AdeleGraziani93 <60431221+AdeleGraziani93@users.noreply.github.com>
2020-01-29 13:26:30 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://REA_BOT.herokuapp.com/execute.php';
+$BOT_TOKEN = 'AAGntrUwrFbIz9zADhicXAnozX6ZryiKkno';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
